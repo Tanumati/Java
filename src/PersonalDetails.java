77e32a3531887b7f12ce805bc43763e2cbae0459
Tanumati <tanoost19@gmail.com>
2019-07-30 19:41:10 +0100

    first commit

---
+++ src/PersonalDetails.java
@@ -0,0 +1,24 @@
+public class PersonalDetails {
+
+   public static void main(String[]args){
+
+    String Name = "Tanumati Patel";
+    int age = 25;
+    String address = "50,Harrow Road,Harrow,HA5 4TT";
+    long mobile = 447533310121L;
+    double salary = 1140.56;
+    System.out.println("Mrs "+Name);
+    System.out.println(age+" years");
+    System.out.println(address);
+    System.out.println(mobile);
+    System.out.println("£"+salary);
+
+
+
+
+
+
+
+   }
+
+}
